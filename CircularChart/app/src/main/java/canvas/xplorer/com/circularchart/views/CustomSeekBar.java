eb7a72fbd1154a24cf4c40f456127a9b10606aa7
chrislucas <christoffer.luccas@gmail.com>
2018-06-29 11:57:09 -0300

    estudo sobre desenho de arcos

--- CircularChart/app/src/main/java/canvas/xplorer/com/circularchart/views/CustomSeekBar.java
+++ CircularChart/app/src/main/java/canvas/xplorer/com/circularchart/views/CustomSeekBar.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
 
 import java.util.Locale;
 
@@ -36,14 +38,28 @@ public class CustomSeekBar extends android.support.v7.widget.AppCompatSeekBar {
     @Override
     protected synchronized void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        float value = (getProgress() * 1.0f / getMax() * 1.0f) * (getWidth() * 1.0f);
+        float value = (float) Math.floor((getProgress() * 1.0f / getMax() * 1.0f) * (getWidth() * 1.0f));
         mPaint.setTextSize(35.0f);
-
         int h = getHeight();
         int b = getBottom();
         int t = getTop();
         int w = getWidth();
-
-        canvas.drawText(String.format(Locale.getDefault(), "%d", getProgress()), value, getHeight(), mPaint);
+        Rect thumb = getThumb().getBounds();
+        Log.i("THUMB_SEEK_BAR"
+                , String.format("Center x: %d, Center y: %d.\nLeft: %d.\nTop: %d." +
+                                "\nRight: %d.\nBottom: %d"
+                    , thumb.centerX()
+                    , thumb.centerY()
+                    , thumb.left
+                    , thumb.top
+                    , thumb.right
+                    , thumb.bottom
+                )
+        );
+        value = value - Math.abs(thumb.left - thumb.right);
+        Log.i("SEEK_BAR"
+                , String.format("%d %d %d %d Value: %f", h, b, t, w, value));
+        canvas.drawText(String.format(Locale.getDefault()
+                , "%d", getProgress()), thumb.centerX(), thumb.centerY() - 60, mPaint);
     }
 }
