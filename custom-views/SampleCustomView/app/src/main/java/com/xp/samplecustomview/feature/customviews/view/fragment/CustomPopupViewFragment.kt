30e567022be2969d2566f750d9117d399a8ae729
christoffer lucas <christoffer.santos@mercadolivre.com>
2021-05-25 16:58:52 -0300

    estudos

---
+++ custom-views/SampleCustomView/app/src/main/java/com/xp/samplecustomview/feature/customviews/view/fragment/CustomPopupViewFragment.kt
@@ -0,0 +1,66 @@
+package com.xp.samplecustomview.feature.customviews.view.fragment
+
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.br.bookcustomview.popupview.BuilderCustomPopupView
+import com.br.bookcustomview.popupview.LayoutParamsDimension
+import com.br.bookcustomview.popupview.OffsetLocation
+import com.xp.samplecustomview.R
+import com.xp.samplecustomview.commons.ext.ownTag
+import com.xp.samplecustomview.helper.fragments.BaseBehaviorFragment
+
+
+class CustomPopupViewFragment : Fragment(), BaseBehaviorFragment {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val viewRoot = inflater.inflate(R.layout.fragment_custom_popup_view, container, false)
+
+
+        viewRoot.findViewById<Button>(R.id.bt_control_custom_view).setOnClickListener {
+            val popupView = BuilderCustomPopupView.create(
+                requireContext(),
+                LayoutParamsDimension(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                ), R.layout.layout_custom_popup_view_1
+            )
+
+            popupView.showAtLocation(viewRoot, OffsetLocation(0, 0, Gravity.CENTER))
+
+            popupView.contentView.findViewById<Button>(R.id.bt_close_popup_view)?.let {
+                it.setOnClickListener {
+                    popupView.dismiss()
+                }
+            }
+        }
+
+
+        return viewRoot
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            CustomPopupViewFragment()
+    }
+
+    override fun getMyOwnTag(): String = javaClass.ownTag
+
+    override fun getInstanceFragment(): Fragment = this
+}
