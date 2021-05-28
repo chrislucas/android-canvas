038b9f0abfdc7f0c968cacf4bf46c1d7b6e2ab91
christoffer lucas <christoffer.santos@mercadolivre.com>
2021-05-28 10:31:04 -0300

    sample

--- custom-views/SampleCustomView/app/src/main/java/com/xp/samplecustomview/feature/customviews/view/fragment/CustomDashedProgressBarFragment.kt
+++ custom-views/SampleCustomView/app/src/main/java/com/xp/samplecustomview/feature/customviews/view/fragment/CustomDashedProgressBarFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import com.xp.samplecustomview.R
 import com.xp.samplecustomview.commons.ext.ownTag
@@ -22,6 +23,25 @@ class CustomDashedProgressBarFragment : Fragment(), BaseBehaviorFragment {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_custom_dashed_progress_bar, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<ProgressBar>(R.id.progressbar).let { progressBar ->
+            Thread {
+                activity?.run {
+                    var counter = 1
+                    while (progressBar.progress < 100) {
+                        this.runOnUiThread {
+                            progressBar.progress = counter
+                        }
+                        Thread.sleep(1000)
+                        counter += 1
+                    }
+
+                }
+            }
+        }
     }
 
     companion object {
