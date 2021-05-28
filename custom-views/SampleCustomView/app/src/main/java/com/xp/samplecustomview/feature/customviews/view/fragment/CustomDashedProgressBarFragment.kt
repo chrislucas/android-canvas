ff450f3fa1fc90784503962714523aa1e8a53290
chrislucas <christoffer.luccas@gmail.com>
2021-05-28 09:10:21 -0300

    corrigindo dependencias de alguns fragments"

--- custom-views/SampleCustomView/app/src/main/java/com/xp/samplecustomview/feature/customviews/view/fragment/CustomDashedProgressBarFragment.kt
+++ custom-views/SampleCustomView/app/src/main/java/com/xp/samplecustomview/feature/customviews/view/fragment/CustomDashedProgressBarFragment.kt
@@ -33,4 +33,5 @@ class CustomDashedProgressBarFragment : Fragment(), BaseBehaviorFragment {
     override fun getMyOwnTag(): String = javaClass.ownTag
 
     override fun getInstanceFragment(): Fragment = this
+    override fun actionOnBackPressedInFragment() {}
 }
