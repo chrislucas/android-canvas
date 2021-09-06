572d34ab80e163d1ab74186c1144f674e0c815a4
christoffer lucas <christoffer.santos@mercadolivre.com>
2021-09-06 09:07:07 -0300

    estudando custom attrs

--- custom-views/SampleCustomView/samplecustomviews/src/main/java/com/xp/samplecustomviews/feature/clickable/ClickableView.kt
+++ custom-views/SampleCustomView/samplecustomviews/src/main/java/com/xp/samplecustomviews/feature/clickable/ClickableView.kt
@@ -31,11 +31,20 @@ class ClickableView : View {
     }
 
     private fun initBehavior(ctx: Context?, attrs: AttributeSet?) {
+        // Obtem os atributos definidos na View via XML ja processados
         val styledAttrs: TypedArray? = ctx?.obtainStyledAttributes(attrs, R.styleable.ClickableView)
 
+        //  Os atributos sao colocados em num Array e o compilador cria constantes de valor INT para
+        // indicar em qual indice esta cada atributo. Cada attr criado em <declare-styleable> vira
+        // um valor no array e respectivamente eh criado uma constante para indicar sua posicao nele
+        // Abaixo verifico se o valor existe no array atraves da constante 'ClickableView_classCreateBehavior'
         val hasAttr = styledAttrs?.hasValue(R.styleable.ClickableView_classCreateBehavior) ?: false
 
         if (hasAttr) {
+            // Se existe entao eu o recupero
+            // como defini que o tipo eh uma string (nome completo de uma classe)
+            // ja utilizo no retorno uma ext function para instanciar uma classe a partir
+            // de seu nome completo: package.NomeDaClasse
             mBehavior = styledAttrs?.getString(R.styleable.ClickableView_classCreateBehavior)
                 ?.fromClassNameToCreateBehavior()
         }
