f5a7da18d22f7aaf04c62c85ac723f6d16f5ced8
christoffer lucas <christoffer.santos@mercadolivre.com>
2023-10-09 09:58:04 -0300

    estudos

---
+++ RadarAnimation/app/src/main/java/com/br/experience/features/beziercurve/drawingheart/DrawingHeartWithBezierCurveActivity.kt
@@ -0,0 +1,15 @@
+package com.br.experience.features.beziercurve.drawingheart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.br.experience.radaranimation.R
+
+/**
+ * http://androidconceptspp.blogspot.com/2015/03/make-heart-shape-custom-view-in-android.html
+ */
+class DrawingHeartWithBezierCurveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drawing_heart_with_bezier_curve)
+    }
+}
